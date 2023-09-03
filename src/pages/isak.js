0bf043e8096b7826fde5a342ec76398e99e1c79a
leaslyh <guerreroxleslie@gmail.com>
2023-09-02 22:42:57 -0500

    restored Isak.js page

---
+++ src/pages/isak.js
@@ -0,0 +1,13 @@
+import React from "react";
+ 
+const Isak = () => {
+    return (
+        <div>
+            <h1>
+                for Luci v3
+            </h1>
+        </div>
+    );
+};
+ 
+export default Isak;
